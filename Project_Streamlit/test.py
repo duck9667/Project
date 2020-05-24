943376728b36fe0a833009b6693b5cd9ba726936
duck <kjh9667@gmail.com>
2020-05-25 00:14:09 +0900

    duck

---
+++ Project_Streamlit/test.py
@@ -0,0 +1,11 @@
+import streamlit as st 
+
+## Title
+st.title("Streamlit Tutorial")
+
+## Header/Subheader
+st.header("This is header")
+st.subheader("This is subheader")
+
+## Text
+st.text("Hello Streamlit! 이 글은 튜토리얼 입니다.")
